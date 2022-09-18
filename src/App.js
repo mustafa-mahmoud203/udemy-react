1e64dc2507eb43a738dfb28a610109da2dca4e75
mustafa-mahmoud203 <must.afa000262@gmail.com>
2022-09-18 13:10:02 +0200

    use json server

--- src/App.js
+++ src/App.js
@@ -3,26 +3,44 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Courses from "./Pages/MainPage";
 import NavHeader from "./Layouts/Nav";
 import Footer from "./Layouts/Footer";
-import jsonData from "./generated.json";
 import CourseDataPage from "./Pages/CourseDataPage";
-import { Route ,Routes} from "react-router-dom";
-import { BrowserRouter as Router } from "react-router-dom"; 
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+
 // import FadingTextCompontent from "./components/sec-page/SeeMore"
 
-const allDataForCours = jsonData;
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState("");
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://localhost:3000/courses")
+      .then((res) => res.json())
+      .then((data) => {
+        setLoading(false);
+        setData(data);
+      })
+      .catch((res) => {
+        setLoading(false);
+        setError(true);
+      });
+  }, []);
   return (
     <>
-    <Router>
-      <NavHeader />
-      <Routes> 
-        <Route path="/" element={<Courses allCourses={allDataForCours} />}></Route>
-        <Route path="/course" element={<CourseDataPage allData={allDataForCours.courses[0]}></CourseDataPage>}></Route>
-       </Routes>
-      <Footer />
-  
-</Router>
-
+      <Router>
+        <NavHeader />
+        <Routes>
+          <Route
+            path="/"
+            element={<Courses data={data} error={error} loading={loading} />}
+          ></Route>
+          {/* <Route path="/course" element={<CourseDataPage allData={allDataForCours.courses[0]}></CourseDataPage>}></Route> */}
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
